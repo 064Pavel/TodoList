267a389212c4e57e0623331012a256c25c903399
LelouchLamperougee <pavel.bv2003@gmail.com>
2023-03-05 16:46:44 +0300

    Project init

---
+++ resources/js/app.js
@@ -0,0 +1,13 @@
+import './bootstrap';
+
+import '../sass/app.scss'
+
+import {createApp} from "vue/dist/vue.esm-bundler";
+import App from '../vue-components/App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
---
+++ resources/vue-components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <button class="btn btn-success">click</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App"
+}
+</script>
+
+<style scoped>
+
+</style>
